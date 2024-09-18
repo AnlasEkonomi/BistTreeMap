cd59624dbce6a3134ebf999072c5f255711edafe
Anlaşılır Ekonomi <anlasilirekonomi@gmail.com>
2024-09-18 22:24:41 +0300

    Update BistTreeMap.py

--- BistTreeMap.py
+++ BistTreeMap.py
@@ -10,7 +10,6 @@ r=requests.get(url).text
 tablo=pd.read_html(StringIO(r))[2]
 sektör=pd.DataFrame({"Hisse":tablo["Kod"],"Sektör":tablo["Sektör"],"Piyasa Değeri (mn $)":tablo["Piyasa Değeri (mn $)"]})
 tablo2=pd.read_html(StringIO(r))[7]
-tablo2["Günlük Getiri (%)"]=pd.to_numeric(tablo2["Günlük Getiri (%)"].str.replace('%', '').str.replace(',', '.'),errors='coerce')
 getiri=pd.DataFrame({"Hisse":tablo2["Kod"],"Getiri (%)":tablo2["Günlük Getiri (%)"]/100})
 df=pd.merge(sektör,getiri,on="Hisse")
 df["Piyasa Değeri (mn $)"]=df["Piyasa Değeri (mn $)"].str.replace('.', '').str.replace(',', '.').astype('float64')
